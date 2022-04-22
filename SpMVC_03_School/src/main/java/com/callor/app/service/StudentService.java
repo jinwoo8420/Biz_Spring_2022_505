974adc50f6b2b9658292372dcfead7f57391b77e
jinwoo8420 <1223wlsdn@naver.com>
2022-04-22 16:45:33 +0900

    0422

---
+++ SpMVC_03_School/src/main/java/com/callor/app/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.callor.app.service;
+
+import java.util.List;
+
+import com.callor.app.domain.StudentVO;
+
+public interface StudentService {
+	public int insert(StudentVO studentVO);
+	public List<StudentVO> selectAll();
+	public StudentVO findbyId(String stNum);
+}
