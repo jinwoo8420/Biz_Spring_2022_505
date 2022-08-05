0630f34ee896ba1ab99e42309837cf9c78c564dd
jinwoo8420 <1223wlsdn@naver.com>
2022-08-05 11:51:41 +0900

    0805

---
+++ SpMVC_050_todoListV2/src/main/java/com/callor/todo/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.callor.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+
+		return "user/login";
+	}
+
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String join() {
+
+		return "user/join";
+	}
+
+	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
+	public String mypage() {
+
+		return "user/mypage";
+	}
+
+}
---
+++ SpMVC_050_todoListV2/src/main/java/com/callor/todo/service/auth/AuthorProviderImpl.java
@@ -0,0 +1,61 @@
+package com.callor.todo.service.auth;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("authenticationProvider")
+public class AuthorProviderImpl implements AuthenticationProvider {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		/*
+		 * getPrincipal().toString() 형식으로 사용 가능하지만
+		 * NullPointerException이 발생 할 수 있으므로 문자열 형변환 사용 권장
+		 */
+
+		String username = (String) authentication.getPrincipal();
+		String password = (String) authentication.getCredentials();
+
+		if (username.equals("jinwoo8420") == false) {
+			throw new UsernameNotFoundException("USERNAME 오류");
+		}
+
+		if (password.equals("1234") == false) {
+			throw new BadCredentialsException("PASSWORD 오류");
+		}
+
+		List<GrantedAuthority> grantList = new ArrayList<>();
+		grantList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		grantList.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+		/*
+		 * 사용자 이름과 비밀번호, 권한 리스트로 Token 발행
+		 */
+
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password,
+				grantList);
+
+		return token;
+	}
+
+	/*
+	 * security에서 인증 절차를 수행하기 위하여 AuthenticationProvider를 상속받으면
+	 * 반드시 supports를 true로 만들어주어야 한다 
+	 */
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return true;
+	}
+
+}
